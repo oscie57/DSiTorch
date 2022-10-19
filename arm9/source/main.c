f357a06b26a003b356c4c0991fbe1c97d661180a
oscie <hudso009@gmail.com>
2022-10-20 00:30:37 +0100

    both screens now work!

--- arm9/source/main.c
+++ arm9/source/main.c
@@ -10,57 +10,66 @@
 
 bool status = false;
 
+int bgTop;
+int bgBot;
+
+
 void turnOn() {
-	printf("writing - torch on");
+	// turn the torch on
 	fifoSendValue32(FIFO_CAMERA, FLASH_ACTIVATE); // turns on torch
 	fifoWaitValue32(FIFO_CAMERA);
 	fifoGetValue32(FIFO_CAMERA);
-	printf("written - torch on");
-	// set backgrounds to top1 and bottom1
 
-	int bgTop = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
+	// set backgrounds to top1 and bottom1
 	dmaCopy(top1Bitmap, bgGetGfxPtr(bgTop), 256*256);
 	dmaCopy(top1Pal, BG_PALETTE, 256*2);
+	dmaCopy(bottom1Bitmap, bgGetGfxPtr(bgBot), 256*256);
+	dmaCopy(bottom1Pal, BG_PALETTE_SUB, 256*2);
 
+	// set status to on
 	status = true;
 }
 
 void turnOff() {
-	printf("writing - torch off");
-	fifoSendValue32(FIFO_CAMERA, FLASH_DEACTIVATE);  // turns off torch
+	// turn the torch off
+	fifoSendValue32(FIFO_CAMERA, FLASH_DEACTIVATE);
 	fifoWaitValue32(FIFO_CAMERA);
 	fifoGetValue32(FIFO_CAMERA);
-	printf("written - torch off");
-	// set backgrounds to top0 and bottom0
 
-	int bgTop = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
+	// set backgrounds to top0 and bottom0
 	dmaCopy(top0Bitmap, bgGetGfxPtr(bgTop), 256*256);
 	dmaCopy(top0Pal, BG_PALETTE, 256*2);
+	dmaCopy(bottom0Bitmap, bgGetGfxPtr(bgBot), 256*256);
+	dmaCopy(bottom0Pal, BG_PALETTE_SUB, 256*2);
 
+	// set status to false
 	status = false;
 }
 
 int main(void) {
 	touchPosition touch;
 
-	videoSetMode(MODE_5_2D);
-    vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
+	vramSetBankA(VRAM_A_MAIN_BG); // for the top
+	vramSetBankC(VRAM_C_SUB_BG); // for the bottom
 
-	consoleDemoInit();  //setup the sub screen for printing
+	videoSetMode(MODE_5_2D);
+	videoSetModeSub(MODE_5_2D);
+    vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
 
 	iprintf("\x1b[2J");
 
 	status = false;
-	// set backgrounds to top0 and bottom0
 
-	int bgTop = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
+	// consoleDemoInit();  //setup the sub screen for printing
+
+	// set backgrounds to top0 and bottom0
+	bgTop = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
+	bgBot = bgInitSub(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
+
 	dmaCopy(top0Bitmap, bgGetGfxPtr(bgTop), 256*256);
 	dmaCopy(top0Pal, BG_PALETTE, 256*2);
-
-	/*int bgBot = bgSubInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
 	dmaCopy(bottom0Bitmap, bgGetGfxPtr(bgBot), 256*256);
-	dmaCopy(bottom
-	0Pal, BG_PALETTE, 256*2);*/
+	dmaCopy(bottom0Pal, BG_PALETTE_SUB, 256*2);
 
 	while(1) {
 
